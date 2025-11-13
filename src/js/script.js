eaa8e0e7d0143207588e07a1635b7d4e4773debe
LinaBenmabrouk <lina.ben_mabrouk@edu.univ-fcomte.fr>
2025-11-13 14:55:16 +0100

    Ouverture/fermeture menu

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
 
 toggle.addEventListener("click", () => {
-  nav.ariaHidden = "false";
-  toggle.ariaExpanded = "true";
+  const isOpen = toggle.getAttribute("aria-expanded") === "true";
+  nav.setAttribute("aria-hidden", String(isOpen)); 
+  toggle.setAttribute("aria-expanded", String(!isOpen)); 
 });
-
